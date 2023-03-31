e5d9ef306d6f93357d37e3a322f60533d5a96eb8
arthursk14 <arthursk14@gmail.com>
2023-03-31 20:03:51 -0300

    new file:   Lista4_cs.jl 	modified:   Lista4.jl

---
+++ Lista4_cs.jl
@@ -0,0 +1,8 @@
+using Distributions, LinearAlgebra, Plots, Random, Statistics, BenchmarkTools, Interpolations, Roots, Dates, NLsolve, Latexify
+
+function aggregate_savings(ρ, σ, μ, r)
+
+    
+
+
+end
